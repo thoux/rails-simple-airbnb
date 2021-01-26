a11659bd8cc89a402a4aa15eb04dda75169e1977
Fabien Berthoux <thoux08@gmail.com>
2021-01-26 20:24:27 +0100

    Commit all modification

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.delete_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 175,
+  number_of_guests: 8
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 40,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'House Close to St Pancras',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 400,
+  number_of_guests: 12
+)
+
+Flat.create!(
+  name: 'Tower Guest Suite',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 250,
+  number_of_guests: 11
+)
